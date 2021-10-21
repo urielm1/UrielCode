bac5c70c071aad929736f22860028e05d65ff3fb
urielm1 <92134805+urielm1@users.noreply.github.com>
2021-10-20 23:14:16 -0700

    Testhere

--- sketch.js
+++ sketch.js
@@ -1,21 +1,37 @@
-var beeImage;
-var beeFont;
-var xloc, yloc;
+var startSignin;
 function preload(){
-  beeImage = loadImage('assets/bee.jpg');
-  beeFont = loadFont('assets/Honeybee.ttf');
+  startSignin = loadImage('assets/Signin.png');
+  homepage = loadImage('assets/Homepage.png');
+  menupage = loadImage('assets/Menupage.png');
+  plantpage = loadImage('assets/Plantpage.png');
+  rosepage = loadImage('assets/RoseFlowerpage.png');
+  notespage = loadImage('assets/Notespage.png');
+  creatnote = loadImage('assets/Creatingnote.png');
+  newnotespage = loadImage('assets/Newnotespage.png');
+  waterMethods = loadImage('assets/WaterMethods.png');
 }
 
 function setup() {
   // put setup code here
-  createCanvas(600,600);
-  xloc = 300;
-  yloc = 200;
-  noCursor();
+  createCanvas(800,800);
 }
 
 function draw() {
   // put drawing code here
-  background(255);
-  image(beeImage, mouseX, mouseY,50,50);
+  background(220);
+  image(startSignin,100,30);
+}
+
+function mousePressed(){
+  startSignin = homepage;
+  homepage = menupage;
+  menupage = plantpage;
+  plantpage = rosepage;
+  rosepage = menupage;
+  menupage = notespage;
+  notespage = creatnote;
+  creatnote = newnotespage;
+  newnotespage = menupage;
+  menupage = waterMethods;
+
 }
